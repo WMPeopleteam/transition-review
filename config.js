8c9dc6a5b04765ab9445fa190346e006f747f7a8
WMPeopleteam <people.wm@gmail.com>
2026-08-20 10:07:22 +0900

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Google Apps Script 웹앱 배포 후 발급되는 URL을 아래에 붙여넣으세요.
-// 예: https://script.google.com/macros/s/AKfycbx.../exec
-// 설정 방법은 README.md의 "3. Apps Script 배포" 참고
-const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbxVe9ulY_-ymlUjnsgvgGg3dwZZIDI9QH26Mb2bAvNasQol_ZSZUOCBLVDO238338_Y/exec'
-};
